f0ccc39c7ba35694008dfdc6bffd7fccbae43cf2
Linu5Frick <linusflorentin.frick@uzh.ch>
2026-04-08 17:07:31 +0200

    Start for map, region and turn class #37

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Map.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Map.java
@@ -1,5 +1,23 @@
 package ch.uzh.ifi.hase.soprafs26.entity;
 
-public class Map{
+import java.io.Serializable;
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "MAP")
+public class Map implements Serializable{
+
+    private List<Region> StartPoints;
+
+    public List<Region> getStartPoints() {
+        return StartPoints;
+    }
+
+    public void setStartPoints(List<Region> startPoints) {
+        StartPoints = startPoints;
+    }
+    
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Region.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Region.java
@@ -1,5 +1,14 @@
 package ch.uzh.ifi.hase.soprafs26.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "REGIONS")
 public class Region {
+
+    private Long BonusAmount;
+
+     
+
 
 }
