0354d793b375135f044ec3fbcccdf5eee0b4c5e5
Linu5Frick <linusflorentin.frick@uzh.ch>
2026-04-08 14:44:41 +0200

    Implemented Class Entity #37

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Game.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Game.java
@@ -1,5 +1,100 @@
 package ch.uzh.ifi.hase.soprafs26.entity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.List;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 
-public class Game {
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "GAME")
+public class Game implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @OneToMany(mappedBy = "game", cascade = CascadeType.ALL) // cascade: when a game gets saved or deleted,the players get saved or deleted as well
+    private List<Player> playerOrder;
+    
+    @OneToOne(cascade = CascadeType.ALL) 
+    private Map map;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Turn currentTurn;
+
+    @Column 
+    private int currentPlayerIndex;
+
+    @Enumerated(EnumType.STRING)
+    private GameStatus status; //either waiting, running or finished
+
+    public Player getCurrentPlayer(){
+        if(playerOrder == null || playerOrder.isEmpty()) {
+            return null;
+        }
+        return playerOrder.get(currentPlayerIndex);
+    }
+
+    public void setPlayerOrder(List<Player> playerOrder) {
+        this.playerOrder = playerOrder;
+
+        if(playerOrder != null) {
+            for (Player player : playerOrder) {
+                player.setGame(this);
+            }
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Player> getPlayerOrder() {
+        return playerOrder;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public Turn getCurrentTurn() {
+        return currentTurn;
+    }
+
+    public void setCurrentTurn(Turn currentTurn) {
+        this.currentTurn = currentTurn;
+    }
+
+    public int getCurrentPlayerIndex() {
+        return currentPlayerIndex;
+    }
+    
+    public void setCurrentPlayerIndex(int currentPlayerIndex) {
+        this.currentPlayerIndex = currentPlayerIndex;
+    }
+
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GameStatus status) {
+        this.status = status;
+    }
+
 
 }
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/GameStatus.java
@@ -0,0 +1,7 @@
+package ch.uzh.ifi.hase.soprafs26.entity;
+
+public enum GameStatus {
+    WAITING,
+    RUNNING,
+    FINISHED
+}
