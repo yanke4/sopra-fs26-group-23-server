84cf2442d97f795d698244879ea39de28a951336
Kevin Yan <kevin.yan@uzh.ch>
2026-05-15 20:18:01 +0200

    bug fix

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Lobby.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Lobby.java
@@ -15,7 +15,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
@@ -32,7 +31,7 @@ public class Lobby implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
     private Long lobbyId;
 
     @Column(nullable = false)
